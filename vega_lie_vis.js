83c79873dc5f131c6370fd3de8c63b0b1d145e51
Tzu-Han <48499652+Tzu-Han@users.noreply.github.com>
2021-10-10 14:42:59 +0800

    Add files via upload

---
+++ vega_lie_vis.js
@@ -0,0 +1,7 @@
+var vg_1 = "top_100_uni_map.json";
+vegaEmbed("#choropleth_map", vg_1).then(function(result) {
+}).catch(console.error);
+var vg_2 = "top200_scatter_plot.json";
+vegaEmbed("#scatter_plot", vg_2).then(function(result) {
+}).catch(console.error);
+
